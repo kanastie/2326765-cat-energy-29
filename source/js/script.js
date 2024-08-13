7677250b49992b6f9cb926aff8e1ddf4ba09bca4
Anastasia Kolmagorova <canastie@gmail.com>
2024-08-13 21:40:11 +0300

    Реализует слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// -- Мобильное меню
+
 let navMain = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".main-nav__toggle");
 
@@ -11,4 +13,96 @@ navToggle.addEventListener("click", function () {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
   }
+});
+
+// -- Слайдер
+
+let slider = document.querySelector('.slider');
+let before = slider.querySelector('.slider__img-wrapper--before');
+let beforeImg = slider.querySelector('.slider__img-wrapper--before img');
+let after = slider.querySelector('.slider__img-wrapper--after');
+let devider = slider.querySelector('slider__delimiter');
+
+const body = document.body;
+
+let isActive = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  let width = slider.offsetWidth;
+  beforeImg.style.width = `${width}px`;
+});
+
+
+const beforeAfterSlider = (x) => {
+  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+
+  if (screen > 320 && screen < 768) {
+    before.style.width = `${shift / 2}px`;
+    devider.style.left = `${shift / 2}%`;
+  } else {
+    before.style.width = `${shift}px`;
+    devider.style.left = `${shift}%`;
+  }
+
+  before.style.backgroundColor = 'transparent';
+  after.style.backgroundColor = 'transparent';
+};
+
+const pauseEvents = (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
+  return false;
+};
+
+slider.addEventListener('mousedown', () => {
+  isActive = true;
+});
+
+slider.addEventListener('mouseup', () => {
+  isActive = false;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isActive = false;
+});
+
+slider.addEventListener('mousemove', (evt) => {
+  if (!isActive) {
+    return;
+  }
+
+  let x = evt.pageX;
+
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(evt);
+});
+
+
+slider.addEventListener('touchstart', () => {
+  isActive = true;
+});
+
+slider.addEventListener('touchend', () => {
+  isActive = false;
+});
+
+slider.addEventListener('touchend', () => {
+  isActive = false;
+});
+
+slider.addEventListener('touchmove', (evt) => {
+  if (!isActive) {
+    return;
+  }
+
+  let x;
+
+  for (let i = 0; i < evt.changeTouches.lenght; i++) {
+    x = evt.changeTouches[i].pageX;
+  }
+
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(evt);
 });
